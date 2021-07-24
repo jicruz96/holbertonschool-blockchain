6fc90ffb9d837ce3a28ae426e7e149a1a08b49f8
jicruz96 <accounts@jicruz.com>
2021-07-23 23:06:20 -0500

    Initial commit of ec_verify

---
+++ crypto/ec_verify.c
@@ -0,0 +1,31 @@
+#include <hblk_crypto.h>
+
+/**
+ * ec_verify - verifies signature of a set of bytes, using a given public key
+ * @key: EC_KEY structure containing public key used to verify the signature
+ * @msg: characters to verify the signature of
+ * @msglen: number of characters
+ * @sig: signature to be checked
+ * Return: 1 if the signature is valid, and 0 otherwise
+ */
+int ec_verify
+(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t const *sig)
+{
+    EC_KEY *tmp;
+    int status;
+
+    if (
+        !key    ||
+        !msg    ||
+        !msglen ||
+        !sig    ||
+        !(tmp = EC_KEY_dup(key)) ||
+        !ECDSA_verify(0, msg, msglen, sig->sig, sig->len, tmp)
+    )
+        status = 0;
+    else
+        status = 1;
+    
+    EC_KEY_free(tmp);
+    return (status);
+}
