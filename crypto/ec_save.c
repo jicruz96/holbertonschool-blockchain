2598c4fa0b14bfe32c5259bb5b1e0ad7f4479f12
jicruz96 <accounts@jicruz.com>
2021-07-23 21:23:54 -0500

    refactor ec_save.c

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -5,6 +5,10 @@
 #include <openssl/pem.h>
 #include <string.h>
 
+
+FILE *open_file(char const *folder, char const *file);
+
+
 /**
  * ec_save - saves an existing EC key pair on the disk.
  * @key: points to the EC key pair to be saved on disk
@@ -13,33 +17,47 @@
  */
 int ec_save(EC_KEY *key, char const *folder)
 {
+	FILE *pub = NULL, *priv = NULL;
 	struct stat st = {0};
-	char *pubkey_file, *privkey_file;
-	FILE *pub, *priv;
 	int status;
 
-	if (stat(folder, &st) == -1 && mkdir(folder, 0700) == -1)
-		return (0);
-
-	pubkey_file = malloc(sizeof(char) * (strlen(folder) + sizeof(PUB_FILENAME)));
-	privkey_file = malloc(sizeof(char) * (strlen(folder) + sizeof(PRI_FILENAME)));
-
-	sprintf(pubkey_file, "%s/%s", folder, PUB_FILENAME);
-	sprintf(pubkey_file, "%s/%s", folder, PRI_FILENAME);
-
-	pub = fopen(pubkey_file, "w");
-	priv = fopen(privkey_file, "w");
 
 	if (
-		!PEM_write_EC_PUBKEY(pub, key) ||
+		(stat(folder, &st) == -1 && mkdir(folder, 0700) == -1) ||
+		!(pub  = open_file(folder, PUB_FILENAME))              ||
+		!(priv = open_file(folder, PRI_FILENAME))              ||
+		!PEM_write_EC_PUBKEY(pub, key)                         ||
 		!PEM_write_ECPrivateKey(priv, key, NULL, NULL, 0, NULL, NULL)
 	)
 		status = 0;
 	else
 		status = 1;
-	fclose(pub);
-	fclose(priv);
-	free(pubkey_file);
-	free(privkey_file);
+
+	if (pub)  fclose(pub);
+	if (priv) fclose(priv);
 	return (status);
+}
+
+/**
+ * open_file - given a file and folder, open file and return reference to it
+ * @folder: file's parent folder
+ * @filename: name of file to open
+ * Return: pointer to opened file or NULL on failure
+ **/
+FILE *open_file(char const *folder, char const *filename)
+{
+	size_t size = sizeof(char) * (strlen(folder) + 1 + strlen(filename) + 1);
+	char *filepath = malloc(size);
+	FILE *file;
+
+
+	if (filepath)
+	{
+		sprintf(filepath, "%s/%s", folder, filename);
+		file = fopen(filepath, "w");
+		free(filepath);
+		return (file);
+	}
+
+	return (NULL);
 }
