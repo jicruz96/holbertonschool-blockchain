092c8edaa5c6567652adb721198b0edb5f23a4fe
jicruz96 <accounts@jicruz.com>
2021-07-31 16:33:11 -0500

    debug block_hash.c

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -9,8 +9,11 @@
 uint8_t
 *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
+	size_t len;
+
 	if (!block || !hash_buf)
 		return (NULL);
 
-	return (sha256((int8_t const *)block->hash, sizeof(block->hash), hash_buf));
+	len = sizeof(block->info) + block->data.len;
+	return (sha256((int8_t const *)block, len, hash_buf));
 }
