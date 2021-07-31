79af7f3b2a767546ae3b464c6e57290f62503512
jicruz96 <accounts@jicruz.com>
2021-07-31 16:22:18 -0500

    Update checkpatch configuration and blockchain_create.c

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -1,6 +1,6 @@
 #include <blockchain.h>
 #include <string.h>
-
+#include <stdio.h>
 
 
 /**
@@ -19,13 +19,13 @@
  **/
 blockchain_t *blockchain_create(void)
 {
-	blockchain_t *blockchain = malloc(sizeof(blockchain_t));
+	blockchain_t *blockchain = calloc(1, sizeof(blockchain_t));
 	block_t *genesis;
 
 	if (!blockchain)
 		return (NULL);
 
-	genesis = malloc(sizeof(block_t));
+	genesis = calloc(1, sizeof(block_t));
 
 	if (!genesis)
 	{
@@ -47,10 +47,13 @@ blockchain_t *blockchain_create(void)
 	genesis->info.timestamp = GENESIS_TIME;
 	genesis->info.nonce = 0;
 	memset(genesis->info.prev_hash, 0, sizeof(genesis->info.prev_hash));
-	memcpy(genesis->data.buffer, GENESIS_DATA, sizeof(GENESIS_DATA) - 1);
-	genesis->data.len = sizeof(GENESIS_DATA) - 1;
-	memcpy(genesis->hash, GENESIS_HASH, sizeof(GENESIS_HASH));
 
+	genesis->data.len = sizeof(GENESIS_DATA) - 1;
+	memset(genesis->data.buffer, 0, sizeof(genesis->data.buffer));
+	memcpy(genesis->data.buffer, GENESIS_DATA, genesis->data.len);
+
+	memset(genesis->hash, 0, sizeof(GENESIS_HASH));
+	memcpy(genesis->hash, GENESIS_HASH, sizeof(GENESIS_HASH));
 	if (llist_add_node(blockchain->chain, genesis, ADD_NODE_FRONT) == -1)
 	{
 		llist_destroy(blockchain->chain, false, NULL);
