d516c9a6dd011fc2837955aedab3f559991bc650
jicruz96 <accounts@jicruz.com>
2021-08-05 18:38:35 -0500

    First commit of block_mine.c

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -6,5 +6,13 @@
  **/
 void block_mine(block_t *block)
 {
-
+    if (!block)
+        return;
+    
+    for (block->info.nonce = 0; 1; block->info.nonce += 1)
+    {
+        block_hash(block, block->hash);
+        if (hash_matches_difficulty(block->hash, block->info.difficulty))
+            break;
+    }
 }
