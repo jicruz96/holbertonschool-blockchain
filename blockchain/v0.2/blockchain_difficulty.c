1f3ecc321d71e251174609c8e8a4a9d69793eb2d
jicruz96 <accounts@jicruz.com>
2021-08-06 11:52:49 -0500

    adjust blockchain_difficulty for Holberton debugging purposes

--- blockchain/v0.2/blockchain_difficulty.c
+++ blockchain/v0.2/blockchain_difficulty.c
@@ -1,6 +1,30 @@
 #include <blockchain.h>
 #include <stdio.h>
 
+/*
+Difficulty adjustment:
+Retrieve the last Block for which an adjustment was made (the Block with index <blockchain_size> - DIFFICULTY_ADJUSTMENT_INTERVAL)
+Compute the expected elapsed time between the two Blocks
+Compute the actual elapsed time
+The difficulty must be incremented if the elapsed time is lower than half the expected elapsed time
+The difficulty must be decremented if the elapsed time is greater than twice the expected elapsed time
+The difficulty should not change otherwise
+*/
+
+
+void adjust_difficulty(blockchain_t const *blockchain, block_t *last_block, uint32_t *difficulty)
+{
+	block_t *last_adjusted_block = llist_get_node_at(blockchain->chain, last_block->info.index - DIFFICULTY_ADJUSTMENT_INTERVAL);
+	uint64_t actual_time = last_block->info.timestamp - last_adjusted_block->info.timestamp;
+
+	printf("block time: %d\nadjustment interval: %d\n[%u] E: %d | A: %lu | lbts: %lu | labits: %lu\n", BLOCK_GENERATION_INTERVAL, DIFFICULTY_ADJUSTMENT_INTERVAL, last_block->info.index, EXPECTED_TIME_BETWEEN_ADJUSTMENTS, actual_time, last_block->info.timestamp, last_adjusted_block->info.timestamp);
+
+	if (actual_time < EXPECTED_TIME_BETWEEN_ADJUSTMENTS / 2)
+		*difficulty += 1;
+	else if (*difficulty && actual_time > EXPECTED_TIME_BETWEEN_ADJUSTMENTS * 2)
+		*difficulty -= 1;
+}
+
 /**
  * blockchain_difficulty - computes difficulty to assign to next block in chain
  * @blockchain: pointer to Blockchain to analyze
@@ -8,29 +32,16 @@
  **/
 uint32_t blockchain_difficulty(blockchain_t const *blockchain)
 {
-	block_t *block, *last_adjusted_block;
-	uint64_t expected_time, actual_time;
+	block_t *block;
+	uint32_t difficulty;
 
 	if (!blockchain || !(block = llist_get_tail(blockchain->chain)))
 		return (0);
 
+	difficulty = block->info.difficulty;
 
-	if (block->info.index == 0 || block->info.index % DIFFICULTY_ADJUSTMENT_INTERVAL)
-		return (block->info.difficulty);
+	if (block->info.index && !(block->info.index % DIFFICULTY_ADJUSTMENT_INTERVAL))
+		adjust_difficulty(blockchain, block, &difficulty);
 
-	if (block->info.index == DIFFICULTY_ADJUSTMENT_INTERVAL)
-		return (block->info.difficulty + 1);
-
-	last_adjusted_block = llist_get_node_at(blockchain->chain, block->info.index - DIFFICULTY_ADJUSTMENT_INTERVAL);
-	expected_time = BLOCK_GENERATION_INTERVAL * (block->info.index - last_adjusted_block->info.index);
-	actual_time = block->info.timestamp - last_adjusted_block->info.timestamp;
-
-
-	if (actual_time < expected_time / 2)
-		return (block->info.difficulty + 1);
-
-	if (actual_time > expected_time * 2)
-		return (block->info.difficulty ? block->info.difficulty - 1 : 0);
-
-	return (block->info.difficulty);
+	return (difficulty);
 }
