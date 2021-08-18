0290038107cd074cd835ff9b974cdb2db003e2ea
jicruz96 <accounts@jicruz.com>
2021-08-18 04:18:27 -0500

    update blockchain serialization

---
+++ blockchain/v0.3/blockchain_deserialize.c
@@ -0,0 +1,221 @@
+#include <blockchain.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+static void bswap(uint8_t *p, size_t size);
+static int read_attr(int fd, int encoding, void *attr, size_t size);
+static llist_t *read_transactions(int fd, int encoding);
+static llist_t *read_inputs(int fd, int encoding, int num_inputs);
+static llist_t *read_outputs(int fd, int encoding, int num_outputs);
+
+static llist_t *read_transactions(int fd, int encoding)
+{
+	uint32_t num_txs, num_inputs, num_outputs;
+	transaction_t *tx;
+	llist_t *list;
+
+	if (!read_attr(fd, encoding, &num_txs, sizeof(num_txs)) || !num_txs || !(list = llist_create(MT_SUPPORT_FALSE)))
+		return (NULL);
+
+	while (num_txs--)
+	{
+		tx = calloc(1, sizeof(transaction_t));
+		llist_add_node(list, tx, ADD_NODE_REAR);
+		if (
+			!read_attr(fd, encoding, &tx->id, sizeof(tx->id)) ||
+			!read_attr(fd, encoding, &num_inputs, sizeof(num_inputs)) ||
+			!read_attr(fd, encoding, &num_outputs, sizeof(num_outputs)) ||
+			!(tx->inputs = read_inputs(fd, encoding, num_inputs)) ||
+			!(tx->outputs = read_outputs(fd, encoding, num_outputs))
+		)
+		{
+			llist_destroy(list, true, (node_dtor_t)transaction_destroy);
+			return (NULL);
+		}
+	}
+
+	return (list);
+}
+
+/**
+ * read_inputs - reads transaction inputs from a serialized blockchain file
+ * @fd: file descriptor
+ * @encoding: encoding (MSB or LSB)
+ * @num_inputs: number of anticipated inputs
+ * Return: pointer to list of transaction inputs or NULL on failure
+ **/
+static llist_t *read_inputs(int fd, int encoding, int num_inputs)
+{
+	llist_t *inputs;
+	tx_in_t *in;
+
+	if (!num_inputs || !(inputs = llist_create(MT_SUPPORT_FALSE)))
+		return (NULL);
+
+	while (num_inputs--)
+	{
+		in = calloc(1, sizeof(tx_in_t));
+		llist_add_node(inputs, in, ADD_NODE_REAR);
+		if (
+			!read_attr(fd, encoding, &in->block_hash, sizeof(in->block_hash)) ||
+			!read_attr(fd, encoding, &in->tx_id, sizeof(in->tx_id)) ||
+			!read_attr(fd, encoding, &in->tx_out_hash, sizeof(in->tx_out_hash)) ||
+			!read_attr(fd, encoding, &in->sig.sig, sizeof(in->sig.sig)) ||
+			!read_attr(fd, encoding, &in->sig.len, sizeof(in->sig.len))
+		)
+		{
+			llist_destroy(inputs, true, NULL);
+			return (NULL);
+		}
+	}
+	return (inputs);
+}
+
+/**
+ * read_outputs - reads transaction outputs from a serialized blockchain file
+ * @fd: file descriptor
+ * @encoding: encoding (MSB or LSB)
+ * @num_outputs: number of anticipated outputs
+ * Return: pointer to list of transaction outputs or NULL on failure
+ **/
+static llist_t *read_outputs(int fd, int encoding, int num_outputs)
+{
+	llist_t *outputs;
+	tx_out_t *out;
+
+	if (!num_outputs || !(outputs = llist_create(MT_SUPPORT_FALSE)))
+		return (NULL);
+
+	while (num_outputs--)
+	{
+		out = calloc(1, sizeof(tx_out_t));
+		llist_add_node(outputs, out, ADD_NODE_REAR);
+		if (
+			!read_attr(fd, encoding, &out->amount, sizeof(out->amount)) ||
+			!read_attr(fd, encoding, &out->pub, sizeof(out->pub)) ||
+			!read_attr(fd, encoding, &out->hash, sizeof(out->hash))
+		)
+		{
+			llist_destroy(outputs, true, NULL);
+			return (NULL);
+		}
+	}
+	return (outputs);
+}
+
+/**
+ * blockchain_deserialize - deserializes a Blockchain from a file
+ * @path: contains the path to a file to load the Blockchain from
+ *     * return NULL if file doesn’t exist or we don't permission to open
+ * Return: pointer to deserialized Blockchain, or NULL upon failure
+ **/
+blockchain_t *blockchain_deserialize(char const *path)
+{
+	int fd = open(path, O_RDONLY);
+	unsigned char buf[sizeof(block_t)];
+	uint8_t encoding;
+	uint32_t num_blocks, num_unspent;
+	blockchain_t *blockchain;
+	block_t *block;
+	unspent_tx_out_t *utxo;
+
+	if (fd == -1)
+		return (NULL);
+
+	if (
+		read(fd, buf, sizeof(HBLK_MAGIC) - 1) < (ssize_t)sizeof(HBLK_MAGIC) - 1   ||
+		memcmp(buf, HBLK_MAGIC, sizeof(HBLK_MAGIC) - 1)                  ||
+		read(fd, buf, sizeof(HBLK_VERSION) - 1) < (ssize_t)sizeof(HBLK_VERSION) - 1 ||
+		!(blockchain = blockchain_create())
+	)
+	{
+		close(fd);
+		return (NULL);
+	}
+
+	block_destroy((block_t *)llist_pop(blockchain->chain));
+	read(fd, &encoding, sizeof(encoding));
+	read_attr(fd, encoding, &num_blocks, sizeof(num_blocks));
+	read_attr(fd, encoding, &num_unspent, sizeof(num_unspent));
+	while (num_blocks--)
+	{
+		block = calloc(1, sizeof(block_t));
+		if (
+			!read_attr(fd, encoding, &block->info.index,      sizeof(block->info.index))      ||
+			!read_attr(fd, encoding, &block->info.difficulty, sizeof(block->info.difficulty)) ||
+			!read_attr(fd, encoding, &block->info.timestamp,  sizeof(block->info.timestamp))  ||
+			!read_attr(fd, encoding, &block->info.nonce,      sizeof(block->info.nonce))      ||
+			!read_attr(fd, encoding,  block->info.prev_hash,  sizeof(block->info.prev_hash))  ||
+			!read_attr(fd, encoding, &block->data.len,        sizeof(block->data.len))        ||
+			!read_attr(fd, encoding,  block->data.buffer,     block->data.len)                ||
+			!read_attr(fd, encoding,  block->hash,            sizeof(block->hash))            ||
+			!(block->transactions = read_transactions(fd, encoding))
+		)
+		{
+			llist_destroy(blockchain->chain, true, (node_dtor_t)block_destroy);
+			free(blockchain);
+			close(fd);
+			return (NULL);
+		}
+		llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
+	}
+	while (num_unspent--)
+	{
+		utxo = calloc(1, sizeof(unspent_tx_out_t));
+		if (
+			!read_attr(fd, encoding, &utxo->block_hash, sizeof(utxo->block_hash)) ||
+			!read_attr(fd, encoding, &utxo->tx_id, sizeof(utxo->tx_id)) ||
+			!read_attr(fd, encoding, &utxo->out.amount, sizeof(utxo->out.amount)) ||
+			!read_attr(fd, encoding, &utxo->out.pub, sizeof(utxo->out.pub)) ||
+			!read_attr(fd, encoding, &utxo->out.hash, sizeof(utxo->out.pub))
+		)
+		{
+			llist_destroy(blockchain->chain, true, (node_dtor_t)block_destroy);
+			llist_destroy(blockchain->unspent, true, NULL);
+			free(blockchain);
+			close(fd);
+			return (NULL);
+		}
+		llist_add_node(blockchain->unspent, utxo, ADD_NODE_REAR);
+	}
+	close(fd);
+	return (blockchain);
+}
+
+/**
+ * read_attr - reads an ELF file attribute from an ELF file
+ * @fd: file descriptor
+ * @encoding: encoding of attribute
+ * @attr: pointer to attribute where value will be stored
+ * @size: size of attribute
+ * Return: 1 on success | 0 on failure
+ **/
+static int read_attr(int fd, int encoding, void *attr, size_t size)
+{
+	if (read(fd, attr, size) != (ssize_t)size)
+		return (0);
+
+	if (encoding == MSB)
+		bswap((uint8_t *)attr, size);
+
+	return (1);
+}
+
+/**
+ * bswap - byte swap
+ * @p: pointer to data
+ * @size: data size
+ **/
+static void bswap(uint8_t *p, size_t size)
+{
+	uint8_t buf[64] = {0};
+	int i;
+
+	for (i = size - 1; i >= 0; i--)
+		buf[size - i - 1] = p[i];
+
+	for (i = 0; i < (int)size; i++)
+		p[i] = buf[i];
+}
---
+++ blockchain/v0.3/blockchain_serialize.c
@@ -0,0 +1,197 @@
+#include <blockchain.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static void bswap(uint8_t *p, size_t size);
+static void write_attr(int fd, int encoding, void *attr, size_t size);
+static void serialize_blocks(int fd, int encoding, llist_t *list);
+static void serialize_unspent(int fd, int encoding, llist_t *list);
+static void serialize_transactions(int fd, int encoding, llist_t *list);
+static void serialize_transaction_inputs(int fd, int encoding, llist_t *list);
+static void serialize_transaction_outputs(int fd, int encoding, llist_t *list);
+
+/**
+ * blockchain_serialize - serializes a Blockchain into a file
+ * @blockchain: points to the Blockchain to be serialized,
+ * @path: contains the path to a file to serialize the Blockchain into
+ *            * If path points to an existing file, it must be overwritten
+ * Return: 0 upon success, or -1 upon failure
+ **/
+int blockchain_serialize(blockchain_t const *blockchain, char const *path)
+{
+	int fd, num_blocks, num_unspent;
+	uint8_t encoding = HBLK_ENDIAN;
+
+	if (!blockchain || !path)
+		return (-1);
+
+	fd = open(path, O_WRONLY | O_CREAT, 0644);
+	if (fd == -1)
+		return (-1);
+
+	num_blocks = llist_size(blockchain->chain);
+	num_unspent = llist_size(blockchain->unspent);
+
+	write(fd, HBLK_MAGIC,   sizeof(HBLK_MAGIC)   - 1);
+	write(fd, HBLK_VERSION, sizeof(HBLK_VERSION) - 1);
+	write(fd, &encoding,    sizeof(encoding));
+	write(fd, &num_blocks,  sizeof(num_blocks));
+	write(fd, &num_unspent, sizeof(num_unspent));
+	serialize_blocks(fd, HBLK_ENDIAN, blockchain->chain);
+	serialize_unspent(fd, HBLK_ENDIAN, blockchain->unspent);
+	close(fd);
+	return (0);
+}
+/**
+ * serialize_blocks - serializes blocks
+ * @fd: file descriptor
+ * @encoding: file encoding
+ * @list: list of blocks
+ **/
+static void serialize_blocks(int fd, int encoding, llist_t *list)
+{
+	block_t *block;
+	int i, num_blocks = llist_size(list), num_txs;
+
+	for (i = 0; i < num_blocks; i++)
+	{
+		block = llist_get_node_at(list, i);
+		num_txs = llist_size(block->transactions);
+		write_attr(fd, encoding, &block->info.index,      sizeof(block->info.index));
+		write_attr(fd, encoding, &block->info.difficulty, sizeof(block->info.difficulty));
+		write_attr(fd, encoding, &block->info.timestamp,  sizeof(block->info.timestamp));
+		write_attr(fd, encoding, &block->info.nonce,      sizeof(block->info.nonce));
+		write_attr(fd, encoding, block->info.prev_hash,   sizeof(block->info.prev_hash));
+		write_attr(fd, encoding, &block->data.len,        sizeof(block->data.len));
+		write_attr(fd, encoding, block->data.buffer,      block->data.len);
+		write_attr(fd, encoding, block->hash,             sizeof(block->hash));
+		write_attr(fd, encoding, &num_txs,                sizeof(num_txs));
+		serialize_transactions(fd, encoding, block->transactions);
+	}
+
+}
+
+/**
+ * serialize_unspent - serializes unspent transaction outputs
+ * @fd: file descriptor
+ * @encoding: file encoding
+ * @list: list of unspent transaction outputs
+ **/
+static void serialize_unspent(int fd, int encoding, llist_t *list)
+{
+	unspent_tx_out_t *utxo;
+	int i, num_unspent = llist_size(list);
+
+	for (i = 0; i < num_unspent; i++)
+	{
+		utxo = llist_get_node_at(list, i);
+		write_attr(fd, encoding, &utxo->block_hash, sizeof(utxo->block_hash));
+		write_attr(fd, encoding, &utxo->tx_id, sizeof(utxo->tx_id));
+		write_attr(fd, encoding, &utxo->out.amount, sizeof(utxo->out.amount));
+		write_attr(fd, encoding, &utxo->out.pub, sizeof(utxo->out.pub));
+		write_attr(fd, encoding, &utxo->out.hash, sizeof(utxo->out.hash));
+	}
+}
+
+/**
+ * serialize_transactions - serializes transactions
+ * @fd: file descriptor
+ * @encoding: file encoding
+ * @list: list of transactions
+ **/
+static void serialize_transactions(int fd, int encoding, llist_t *list)
+{
+	transaction_t *tx;
+	int i, num_txs, num_inputs, num_outputs;
+
+	num_txs = llist_size(list);
+	for (i = 0; i < num_txs; i++)
+	{
+		tx = llist_get_node_at(list, i);
+		num_inputs = llist_size(tx->inputs);
+		num_outputs = llist_size(tx->outputs);
+		write_attr(fd, encoding, &tx->id, sizeof(tx->id));
+		write_attr(fd, encoding, &num_inputs, sizeof(num_inputs));
+		write_attr(fd, encoding, &num_outputs, sizeof(num_outputs));
+		serialize_transaction_inputs(fd, encoding, tx->inputs);
+		serialize_transaction_outputs(fd, encoding, tx->outputs);
+	}
+}
+
+/**
+ * serialize_transaction_inputs - serializes transaction inputs
+ * @fd: file descriptor
+ * @encoding: file encoding
+ * @list: list of transactions
+ **/
+static void serialize_transaction_inputs(int fd, int encoding, llist_t *list)
+{
+	tx_in_t *in;
+	int i, num_inputs;
+
+	num_inputs = llist_size(list);
+	for (i = 0; i < num_inputs; i++)
+	{
+		in = llist_get_node_at(list, i);
+		write_attr(fd, encoding, &in->block_hash, sizeof(in->block_hash));
+		write_attr(fd, encoding, &in->tx_id, sizeof(in->tx_id));
+		write_attr(fd, encoding, &in->tx_out_hash, sizeof(in->tx_out_hash));
+		write_attr(fd, encoding, &in->sig.sig, sizeof(in->sig.sig));
+		write_attr(fd, encoding, &in->sig.len, sizeof(in->sig.len));
+	}
+}
+
+/**
+ * serialize_transaction_outputs - serializes transaction outputs
+ * @fd: file descriptor
+ * @encoding: file encoding
+ * @list: list of transactions
+ **/
+static void serialize_transaction_outputs(int fd, int encoding, llist_t *list)
+{
+	tx_out_t *out;
+	int i, num_outputs;
+
+	num_outputs = llist_size(list);
+	for (i = 0; i < num_outputs; i++)
+	{
+		out = llist_get_node_at(list, i);
+		write_attr(fd, encoding, &out->amount, sizeof(out->amount));
+		write_attr(fd, encoding, &out->pub, sizeof(out->pub));
+		write_attr(fd, encoding, &out->hash, sizeof(out->pub));
+	}
+}
+
+
+/**
+ * write_attr - writes an attribute into a file in the proper encoding
+ * @fd: file descriptor
+ * @encoding: file encoding
+ * @attr: attribute to write
+ * @size: size of attribute
+ **/
+void write_attr(int fd, int encoding, void *attr, size_t size)
+{
+	if (encoding == MSB)
+		bswap((uint8_t *)attr, size);
+
+	write(fd, attr, size);
+}
+
+/**
+ * bswap - byte swap
+ * @p: pointer to data
+ * @size: data size
+ **/
+static void bswap(uint8_t *p, size_t size)
+{
+	uint8_t buf[64] = {0};
+	int i;
+
+	for (i = size - 1; i >= 0; i--)
+		buf[size - i - 1] = p[i];
+
+	for (i = 0; i < (int)size; i++)
+		p[i] = buf[i];
+}
